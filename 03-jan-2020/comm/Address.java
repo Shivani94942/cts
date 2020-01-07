cf1e8f9b6976c8ec948a5d23c646b1dfa2c73dba
Alchemy <Alchemy@192.168.1.220>
2020-01-07 12:15:01 -0800

    commit

---
+++ 03-jan-2020/comm/Address.java
@@ -0,0 +1,19 @@
+package comm;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Address {
+	private String addressLine1;
+	private String addressLine2;
+	private  int phoneNumber;
+
+}
---
+++ 07-jan-2020/src/comm/Box.java
@@ -0,0 +1,16 @@
+package comm;
+
+public class Box {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int pInt=420;
+		Integer wInt=pInt;//autoboxing
+		int p2=wInt;//autounboxing
+		System.out.println(wInt);
+		System.out.println(p2);
+		
+	}
+
+}
