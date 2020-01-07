59d33ffdb20a024cbc73133ddf0ad109ef6397ee
Alchemy <Alchemy@192.168.1.220>
2020-01-07 12:52:19 -0800

    commit

---
+++ 06-jan-2020/comm/Flyer.java
@@ -0,0 +1,29 @@
+package comm;
+
+public class Flyer {
+	private boolean canFly;
+
+	
+	public Flyer() {
+		super();
+	}
+
+	public Flyer(boolean canFly) {
+		
+		this.canFly = canFly;
+	}
+
+	public void takeOff()
+	{
+		System.out.println("Flyer Take off");
+	}
+
+	public void fly()
+	{
+		System.out.println("flyer fly");
+	}
+	public void land()
+	{
+		System.out.println("flyer land.");
+	}
+}
