440c99342c3b5111da63f8a5c37d23bfb33f5602
Alchemy <Alchemy@192.168.1.220>
2020-01-09 14:49:46 -0800

    commit

--- ToDoCrude_09-01/src/comm/ToDoDao.java
+++ ToDoCrude_09-01/src/comm/ToDoDao.java
@@ -7,4 +7,5 @@ public interface ToDoDao {
 	public ToDo createToDo(String todoId,String author,String taskNAme);
 	public List<ToDo> getAllToDos();
 	public ToDo findById(String todoname);
+	public void deleteToDo(String todoId);
 }
