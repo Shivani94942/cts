ae89652fe679e5a02804e96724b8c753977f2036
Alchemy <Alchemy@192.168.1.220>
2020-01-11 12:34:02 -0800

    commit

---
+++ 11-jan-2020/src/comm/RunnableThreadTest2.java
@@ -0,0 +1,15 @@
+package comm;
+
+public class RunnableThreadTest2 {
+    
+    public static void main(String args[]) {
+        
+        // Since the constructor of the PrintNameRunnable
+        // object creates a Thread object and starts it,
+        // there is no need to do it here.
+        new PrintNameRunnable("Thread1");
+        
+        new PrintNameRunnable("Thread2");
+        new PrintNameRunnable("thread3");
+    }
+}
