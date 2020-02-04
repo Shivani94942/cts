9169e05677078353c0dd4c435f6b60408f9416f4
Alchemy <Alchemy@192.168.1.220>
2020-02-04 09:56:37 -0800

    commit

---
+++ springboot/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into student(first_name,last_name,email) values('meera','pandya','meera@mail.com');
+insert into student(first_name,last_name,email) values('mary','williams','mary@mail.com');
